36576061b4e150f04e17af536bd9345e23b52880
william <goodwong@foxmail.com>
2017-05-11 08:45:04 +0000

    update repository

--- src/Repositories/WechatUserRepository.php
+++ src/Repositories/WechatUserRepository.php
@@ -15,4 +15,23 @@ class WechatUserRepository extends BaseRepository
     {
         return \Goodwong\LaravelWechat\Entities\WechatUser::class;
     }
+
+    /**
+     * Retrieve first data of repository, or create new Entity
+     *
+     * @param array $attributes
+     * @param array $additions
+     * @return mixed
+     */
+    public function firstOrCreate(array $attributes = [], array $additions = [])
+    {
+        $this->applyCriteria();
+        $this->applyScope();
+        $temporarySkipPresenter = $this->skipPresenter;
+        $this->skipPresenter(true);
+        $model = $this->model->firstOrCreate($attributes, $additions);
+        $this->skipPresenter($temporarySkipPresenter);
+        $this->resetModel();
+        return $this->parserResult($model);
+    }
 }

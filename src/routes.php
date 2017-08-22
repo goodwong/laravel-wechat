6d2e3700be3459197b1c500b7c5315f2b5dcd251
william <goodwong@foxmail.com>
2017-08-22 19:05:34 +0800

    add wechat menu api

--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,5 @@ Route::group([
     'namespace' => 'Goodwong\LaravelWechat\Http\Controllers',
 ], function () {
     Route::resource('wechat-users', 'WechatUserController');
+    Route::resource('wechat-menus', 'WechatMenuController');
 });
